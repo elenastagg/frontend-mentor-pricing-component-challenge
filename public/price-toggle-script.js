673a6fc2fbdd35d314e2c945fa9a4e14f223517b
Elena Stagg <elenagracestagg@hotmail.co.uk>
2020-05-05 10:44:53 +0100

    Created desktop version - Still need to add background - Created price toggle script using if statement to change the price changes

---
+++ public/price-toggle-script.js
@@ -0,0 +1,22 @@
+function check() {
+  const basicPrice = document.getElementById("basicPrice");
+  const professionalPrice = document.getElementById("professionalPrice");
+  const masterPrice = document.getElementById("masterPrice");
+
+  if (checkBox.checked == true) {
+    basicPrice.innerText = "$19.99";
+    professionalPrice.innerText = "$24.99";
+    masterPrice.innerText = "$39.99";
+    console.log(checkBox.checked);
+  } else {
+    basicPrice.innerText = "$199.99";
+    professionalPrice.innerText = "$249.99";
+    masterPrice.innerText = "$399.99";
+
+    console.log(checkBox.checked);
+  }
+}
+
+const checkBox = document.getElementById("checkBox");
+
+checkBox.addEventListener("click", check);
